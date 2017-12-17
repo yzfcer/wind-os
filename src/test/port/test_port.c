3440cdd1ec7fc01444c684b0be6c9a49d165256f
yzfcer <yzfcer@163.com>
2017-12-17 16:19:22 +0800

    代码整理

--- src/test/port/test_port.c
+++ src/test/port/test_port.c
@@ -4,11 +4,13 @@
 DECLARE_SUITE(TestSuite1);
 DECLARE_SUITE(TestSuite2);
 DECLARE_SUITE(mpool_test);
+DECLARE_SUITE(tree_test);
 void test_suite_register_all(void)
 {
     REGSTER_SUITE(TestSuite1);
     REGSTER_SUITE(TestSuite2);
     REGSTER_SUITE(mpool_test);
+    REGSTER_SUITE(tree_test);
 }
 
 
