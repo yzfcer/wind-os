1d3e883086e968653817a6f841cfc77b73ebaaff
yzfcer <yzfcer@163.com>
2018-01-13 12:34:23 +0800

    删除无关用例

--- src/test/port/test_port.c
+++ src/test/port/test_port.c
@@ -1,8 +1,8 @@
 #include "test_port.h"
 #include "cut.h"
 
-DECLARE_SUITE(TestSuite1);
-DECLARE_SUITE(TestSuite2);
+//DECLARE_SUITE(TestSuite1);
+//DECLARE_SUITE(TestSuite2);
 DECLARE_SUITE(test_mpool);
 DECLARE_SUITE(test_tree);
 DECLARE_SUITE(test_lock);
@@ -11,8 +11,8 @@ DECLARE_SUITE(test_pipe);
 
 void test_suite_register_all(void)
 {
-    REGSTER_SUITE(TestSuite1);
-    REGSTER_SUITE(TestSuite2);
+    //REGSTER_SUITE(TestSuite1);
+    //REGSTER_SUITE(TestSuite2);
     REGSTER_SUITE(test_mpool);
     REGSTER_SUITE(test_tree);
     REGSTER_SUITE(test_lock);
