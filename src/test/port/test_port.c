ac646f8045907102a30708000926921888404d88
yzfcer <yzfcer@163.com>
2018-05-05 10:29:20 +0800

    代码整理

--- src/test/port/test_port.c
+++ src/test/port/test_port.c
@@ -3,7 +3,7 @@
 
 //DECLARE_SUITE(TestSuite1);
 //DECLARE_SUITE(TestSuite2);
-DECLARE_SUITE(test_mpool);
+DECLARE_SUITE(test_pool);
 DECLARE_SUITE(test_tree);
 #if WIND_MUTEX_SUPPORT
 DECLARE_SUITE(test_mutex);
@@ -25,7 +25,7 @@ void test_suite_register_all(void)
 {
     //REGSTER_SUITE(TestSuite1);
     //REGSTER_SUITE(TestSuite2);
-    REGSTER_SUITE(test_mpool);
+    REGSTER_SUITE(test_pool);
     REGSTER_SUITE(test_tree);
 #if WIND_MUTEX_SUPPORT
     REGSTER_SUITE(test_mutex);
