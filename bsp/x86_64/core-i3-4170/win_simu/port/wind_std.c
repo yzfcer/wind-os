446a04b603324f85f4e2e4be9e140bea0ce4c57d
yzfcer <yzfcer@163.com>
2019-07-04 00:03:08 +0800

    代码整理

--- bsp/x86_64/core-i3-4170/win_simu/port/wind_std.c
+++ bsp/x86_64/core-i3-4170/win_simu/port/wind_std.c
@@ -25,17 +25,34 @@
 //#include "boot_config.h"
 #include <stdio.h>
 #include <conio.h>
+#include <windows.h>
 #include "wind_type.h"
 #include "wind_std.h"
 #include "wind_debug.h"
 
+static void set_scr_buffer(void)
+{
+    HANDLE hConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD BuffSize;
+    BuffSize.X = 100;
+    BuffSize.Y = 3000;
+    SetConsoleScreenBufferSize( hConsoleHandle, BuffSize );
+}
+static void display_cursor(void)
+{
+    CONSOLE_CURSOR_INFO cursor_info = {100, 1}; //1表示显示
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);	
+}
+
 void _wind_std_init(void)
 {
 	w_uint8_t buff;
-	system("title wind-os");
+    system("title wind-os");
 	system("cls");//清理屏幕，准备写入
 	system("mode con cols=100 lines=50");//设置窗口大小
 	system("color 00");//设置颜色
+	set_scr_buffer();
+    display_cursor();
 	while(wind_std_input(&buff,1));
 }
 
