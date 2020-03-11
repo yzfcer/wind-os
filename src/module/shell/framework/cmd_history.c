d3461e48d96ece352e5f724e96268b884110a125
yzfcer <yzfcer@163.com>
2020-03-11 22:17:14 +0800

    代码整理

--- src/module/shell/framework/cmd_history.c
+++ src/module/shell/framework/cmd_history.c
@@ -31,18 +31,21 @@
 extern "C" {
 #endif
 
-void cmd_history_print(w_cmd_his_s *his)
+w_err_t cmd_history_print(w_cmd_his_s *his)
 {
     int i;
+    WIND_ASSERT_RETURN(his != W_NULL,W_ERR_PTR_NULL);
     for(i = 0;i < CMD_HISTORY_COUNT;i ++)
     {
         wind_printf("cmd:%s\r\n",his->hiscmd[i]);
     }
+    return W_ERR_OK;
 }
 
 w_err_t cmd_history_init(w_cmd_his_s *his)
 {
     w_int32_t i;
+    WIND_ASSERT_RETURN(his != W_NULL,W_ERR_PTR_NULL);
     his->hiscnt = 0;
     his->buf_used = 0;
     his->curidx = -1;
@@ -57,6 +60,8 @@ w_err_t cmd_history_init(w_cmd_his_s *his)
 static w_int32_t get_cmd_index(w_cmd_his_s *his,char *cmd)
 {
     w_int32_t i;
+    WIND_ASSERT_RETURN(his != W_NULL,-1);
+    WIND_ASSERT_RETURN(cmd != W_NULL,-1);
     for(i = 0;i < his->hiscnt;i ++)
     {
         if(wind_strcmp(his->hiscmd[i],cmd) == 0)
@@ -68,9 +73,8 @@ static w_int32_t get_cmd_index(w_cmd_his_s *his,char *cmd)
 static w_err_t remove_cmd_index(w_cmd_his_s *his,w_int32_t index)
 {
     w_int32_t i,cmdlen,mvlen;
+    WIND_ASSERT_RETURN(his != W_NULL,-1);
     cmdlen = wind_strlen(his->hiscmd[index])+1;
-    //mvlen = ((w_addr_t)&his->hiscmd[0]) + CMD_HISBUF_LENTH;
-    //mvlen -= (((w_uint32_t)&his->hiscmd[index]) + cmdlen);
 	mvlen = (CMD_HISBUF_LENTH - cmdlen) - (w_int32_t)((w_addr_t)&his->hiscmd[index] - (w_addr_t)&his->hiscmd[0]);
     wind_memcpy(his->hiscmd[index],his->hiscmd[index]+cmdlen,mvlen);
     for(i = index;i < his->hiscnt - 1 ;i ++)
@@ -119,28 +123,32 @@ w_err_t cmd_history_append(w_cmd_his_s *his,char *cmd)
     w_err_t err;
     w_int32_t rest;
     w_int32_t len;
-    if(his == W_NULL || cmd == W_NULL)
-        return W_ERR_PTR_NULL;
-    if(cmd[0] == 0 || cmd[0] == 0x1b)
-        return W_ERR_INVALID;
-    remove_exist_cmd(his,cmd);
+    WIND_ASSERT_RETURN(his != W_NULL,W_ERR_PTR_NULL);
+    WIND_ASSERT_RETURN(cmd != W_NULL,W_ERR_PTR_NULL);
+    WIND_CHECK_RETURN(cmd[0] != 0,W_ERR_INVALID);
+    WIND_CHECK_RETURN(cmd[0] != 0x1b,W_ERR_INVALID);
     len = wind_strlen(cmd);
+    WIND_CHECK_RETURN(len < CMD_HISBUF_LENTH,W_ERR_FAIL);
+    
+    remove_exist_cmd(his,cmd);
     if(his->hiscnt >= CMD_HISTORY_COUNT)
         remove_cmd_index(his,0);
     rest = CMD_HISBUF_LENTH - his->buf_used;
     while(rest < len + 1)
     {
         err = remove_cmd_index(his,0);
-        if(err != W_ERR_OK)
-            return err;
+        WIND_ASSERT_RETURN(err == W_ERR_OK,err);
         rest = CMD_HISBUF_LENTH - his->buf_used;
     }
     add_hiscmd(his,cmd);
+    //cmd_history_print(his);
     return W_ERR_FAIL;
 }
 
 w_err_t cmd_history_get_next(w_cmd_his_s *his,char *cmd)
 {
+    WIND_ASSERT_RETURN(his != W_NULL,W_ERR_PTR_NULL);
+    WIND_ASSERT_RETURN(cmd != W_NULL,W_ERR_PTR_NULL);
     if(his->curidx < his->hiscnt - 1)
         his->curidx += 1;
     if(his->curidx >= his->hiscnt)
@@ -151,8 +159,7 @@ w_err_t cmd_history_get_next(w_cmd_his_s *his,char *cmd)
         return W_ERR_FAIL;
     }
     his->curcmd = his->hiscmd[his->curidx];
-    if(his->curcmd == W_NULL)
-        return W_ERR_FAIL;
+    WIND_CHECK_RETURN(his->curcmd != W_NULL,W_ERR_FAIL);
     wind_strcpy(cmd,his->curcmd);
     return W_ERR_OK;
 }
@@ -160,6 +167,8 @@ w_err_t cmd_history_get_next(w_cmd_his_s *his,char *cmd)
 w_err_t cmd_history_get_prev(w_cmd_his_s *his,char *cmd)
 {
     
+    WIND_ASSERT_RETURN(his != W_NULL,W_ERR_PTR_NULL);
+    WIND_ASSERT_RETURN(cmd != W_NULL,W_ERR_PTR_NULL);
     if(his->curidx < 0)
     {
         his->curidx = -1;
@@ -168,8 +177,7 @@ w_err_t cmd_history_get_prev(w_cmd_his_s *his,char *cmd)
         return W_ERR_FAIL;
     }
     his->curcmd = his->hiscmd[his->curidx];
-    if(his->curcmd == W_NULL)
-        return W_ERR_FAIL;
+    WIND_CHECK_RETURN(his->curcmd != W_NULL,W_ERR_FAIL);
     wind_strcpy(cmd,his->curcmd);
     if(his->curidx > 0)
         his->curidx -= 1;
