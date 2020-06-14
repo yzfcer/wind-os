18a2b188d1797d0e65007127c94b37e41067b698
yzfcer <yzfcer@163.com>
2020-06-15 00:02:10 +0800

    代码整理

--- src/module/net/wind_netnode.c
+++ src/module/net/wind_netnode.c
@@ -23,11 +23,49 @@
 **------------------------------------------------------------------------------------------------------
 *******************************************************************************************************/
 #include "wind_netnode.h"
+#include "wind_debug.h"
 #ifdef __cplusplus
 extern "C" {
 #endif // #ifdef __cplusplus
+static w_dlist_s netnode_list;
+w_err_t _wind_netnode_mod_init(void)
+{
+    DLIST_INIT(netnode_list);
+    return W_ERR_OK;
+}
 
+w_netnode_s* wind_netnode_get(char *name)
+{
+    return wind_obj_get(name,&netnode_list)
+}
 
+w_err_t wind_netnode_register(w_netnode_s *netnode)
+{
+    WIND_ASSERT_RETURN(netnode != W_NULL,W_ERR_PTR_NULL);
+    return wind_obj_register(&netnode->obj,&netnode_list);
+}
+
+w_err_t wind_netnode_unregister(w_netnode_s *netnode)
+{
+    WIND_ASSERT_RETURN(netnode != W_NULL,W_ERR_PTR_NULL);
+    return wind_obj_unregister(&netnode->obj,&netnode_list);
+}
+
+w_err_t wind_netnode_enable(w_netnode_s *netnode)
+{
+    WIND_ASSERT_RETURN(netnode != W_NULL,W_ERR_PTR_NULL);
+    WIND_ASSERT_RETURN(netnode->obj.magic == WIND_NETNODE_MAGIC,W_ERR_INVALID);
+    SET_F_NETNODE_ENABLE(netnode);
+    return W_ERR_OK;
+}
+
+w_err_t wind_netnode_disable(w_netnode_s *netnode)
+{
+    WIND_ASSERT_RETURN(netnode != W_NULL,W_ERR_PTR_NULL);
+    WIND_ASSERT_RETURN(netnode->obj.magic == WIND_NETNODE_MAGIC,W_ERR_INVALID);
+    CLR_F_NETNODE_ENABLE(netnode);
+    return W_ERR_OK;
+}
 
 
 #ifdef __cplusplus
--- src/module/net/wind_netnode.h
+++ src/module/net/wind_netnode.h
@@ -24,14 +24,34 @@
 *******************************************************************************************************/
 #include "wind_config.h"
 #include "wind_type.h"
+#include "wind_obj.h"
 #ifndef WIND_NETNODE_H__
 #define WIND_NETNODE_H__
 #ifdef __cplusplus
 extern "C" {
 #endif // #ifdef __cplusplus
+#define WIND_NETNODE_MAGIC 0x35FA518C
 
+#define F_NETNODE_ENABLE (0x01 << 0) //Mark whether the netnode object is enable
+#define IS_F_NETNODE_ENABLE(netnode) ((netnode->obj.flag & F_NETNODE_ENABLE) == F_NETNODE_ENABLE)
+#define SET_F_NETNODE_ENABLE(netnode) (netnode->obj.flag |= F_NETNODE_ENABLE)
+#define CLR_F_NETNODE_ENABLE(netnode) (netnode->obj.flag &= (~F_NETNODE_ENABLE))
 
+typedef struct __w_netnode_s w_netnode_s;
+struct __w_netnode_s
+{
+    w_obj_s obj;
+    w_err_t (*input)(w_netnode_s *netnode,void *package);
+    w_err_t (*output)(w_netnode_s *netnode,void *package);
+};
 
+w_err_t _wind_netnode_mod_init(void);
+w_netnode_s* wind_netnode_get(char *name);
+
+w_err_t wind_netnode_register(w_netnode_s *netnode);
+w_err_t wind_netnode_unregister(w_netnode_s *netnode);
+w_err_t wind_netnode_enable(w_netnode_s *netnode);
+w_err_t wind_netnode_disable(w_netnode_s *netnode);
 
 #ifdef __cplusplus
 }
