4a11d5e84be64e1b3f2a1ea964039d3438a002b7
yzfcer <yzfcer@163.com>
2020-06-14 23:27:56 +0800

    代码整理

---
+++ src/module/net/wind_netnode.h
@@ -0,0 +1,40 @@
+/****************************************Copyright (c)**************************************************
+**                                       God's harbor
+**
+**                                       yzfcer@163.com
+**
+**--------------File infomation-------------------------------------------------------------------------
+** FileName    : wind_netnode.h
+** Author      : Jason Zhou
+** Last Date   : 2020-06-14
+** Description : 
+**              
+**--------------History---------------------------------------------------------------------------------
+** Author      : Jason Zhou
+** Version     : v1.0
+** Date        : 2020-06-14
+** Description : First version
+**
+**--------------Cureent version-------------------------------------------------------------------------
+** Modify      : Jason Zhou
+** Date        : 2020-06-14
+** Description : 
+**
+**------------------------------------------------------------------------------------------------------
+*******************************************************************************************************/
+#include "wind_config.h"
+#include "wind_type.h"
+#ifndef WIND_NETNODE_H__
+#define WIND_NETNODE_H__
+#ifdef __cplusplus
+extern "C" {
+#endif // #ifdef __cplusplus
+
+
+
+
+#ifdef __cplusplus
+}
+#endif // #ifdef __cplusplus
+#endif //#ifndef WIND_NETNODE_H__
+
