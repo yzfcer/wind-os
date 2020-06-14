524a6a334706fc9fd37168339e5df25f7eb41458
yzfcer <yzfcer@163.com>
2020-06-15 00:06:20 +0800

    代码整理

---
+++ src/module/net/wind_skb.h
@@ -0,0 +1,40 @@
+/****************************************Copyright (c)**************************************************
+**                                       God's harbor
+**
+**                                       yzfcer@163.com
+**
+**--------------File infomation-------------------------------------------------------------------------
+** FileName    : wind_skb.h
+** Author      : Jason Zhou
+** Last Date   : 2020-06-15
+** Description : 
+**              
+**--------------History---------------------------------------------------------------------------------
+** Author      : Jason Zhou
+** Version     : v1.0
+** Date        : 2020-06-15
+** Description : First version
+**
+**--------------Cureent version-------------------------------------------------------------------------
+** Modify      : Jason Zhou
+** Date        : 2020-06-15
+** Description : 
+**
+**------------------------------------------------------------------------------------------------------
+*******************************************************************************************************/
+#include "wind_config.h"
+#include "wind_type.h"
+#ifndef WIND_SKB_H__
+#define WIND_SKB_H__
+#ifdef __cplusplus
+extern "C" {
+#endif // #ifdef __cplusplus
+
+
+
+
+#ifdef __cplusplus
+}
+#endif // #ifdef __cplusplus
+#endif //#ifndef WIND_SKB_H__
+
