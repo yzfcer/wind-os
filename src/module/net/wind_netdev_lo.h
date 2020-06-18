7abd7988f7e4d0150d24d18ad519d8a7418e3513
yzfcer <yzfcer@163.com>
2020-06-18 22:13:40 +0800

    代码整理

---
+++ src/module/net/wind_netdev_lo.h
@@ -0,0 +1,40 @@
+/****************************************Copyright (c)**************************************************
+**                                       God's harbor
+**
+**                                       yzfcer@163.com
+**
+**--------------File infomation-------------------------------------------------------------------------
+** FileName    : wind_netdev_lo.h
+** Author      : Jason Zhou
+** Last Date   : 2020-06-18
+** Description : 
+**              
+**--------------History---------------------------------------------------------------------------------
+** Author      : Jason Zhou
+** Version     : v1.0
+** Date        : 2020-06-18
+** Description : First version
+**
+**--------------Cureent version-------------------------------------------------------------------------
+** Modify      : Jason Zhou
+** Date        : 2020-06-18
+** Description : 
+**
+**------------------------------------------------------------------------------------------------------
+*******************************************************************************************************/
+#ifndef WIND_NETDEV_LO_H__
+#define WIND_NETDEV_LO_H__
+#include "wind_config.h"
+#include "wind_type.h"
+#ifdef __cplusplus
+extern "C" {
+#endif // #ifdef __cplusplus
+
+
+
+
+#ifdef __cplusplus
+}
+#endif // #ifdef __cplusplus
+#endif //#ifndef WIND_NETDEV_LO_H__
+
---
+++ src/module/net/wind_route_tb.h
@@ -0,0 +1,40 @@
+/****************************************Copyright (c)**************************************************
+**                                       God's harbor
+**
+**                                       yzfcer@163.com
+**
+**--------------File infomation-------------------------------------------------------------------------
+** FileName    : wind_route_tb.h
+** Author      : Jason Zhou
+** Last Date   : 2020-06-18
+** Description : 
+**              
+**--------------History---------------------------------------------------------------------------------
+** Author      : Jason Zhou
+** Version     : v1.0
+** Date        : 2020-06-18
+** Description : First version
+**
+**--------------Cureent version-------------------------------------------------------------------------
+** Modify      : Jason Zhou
+** Date        : 2020-06-18
+** Description : 
+**
+**------------------------------------------------------------------------------------------------------
+*******************************************************************************************************/
+#ifndef WIND_ROUTE_TB_H__
+#define WIND_ROUTE_TB_H__
+#include "wind_config.h"
+#include "wind_type.h"
+#ifdef __cplusplus
+extern "C" {
+#endif // #ifdef __cplusplus
+
+
+
+
+#ifdef __cplusplus
+}
+#endif // #ifdef __cplusplus
+#endif //#ifndef WIND_ROUTE_TB_H__
+
